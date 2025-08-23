5b0542cb146025b12d2455683dd04312dcc54b33
whtft <whatafairfooot@gmail.com>
2025-08-23 20:53:01 +0200

    refactor, classes and shit

--- index.js
+++ index.js
@@ -3,16 +3,15 @@ const max_buffer = 16_000_000
 
 const main = document.querySelector('article main')
 const fileInput = document.querySelector('#fileInput')
-const PBAR = document.querySelector('#progress')
-const progressInfo = document.querySelector('#progressInfo')
 const LOCATION = new URL(location.href)
 const rid = new URLSearchParams(LOCATION.search).get('code')
 window.history.replaceState({}, document.title, LOCATION.pathname)
 
 if (navigator.wakeLock) navigator.wakeLock.request('screen')
 
-let filebuffer, received, ts
-let connection = null
+let connection
+/** @type {FileTransfer} */ let SND
+/** @type {FileTransfer} */ let REC
 
 const peer = new Peer().on('open', onPeerOpen).on('connection', onPeerConnection)
 
@@ -33,6 +32,9 @@ function onPeerConnection(conn) {
         .on('close', () => {
             fileInput.disabled = true
             console.log('CONNECTION CLOSED')
+            if (main.matches(':empty')) {
+                appendQRCode(`${LOCATION.origin + LOCATION.pathname}?code=${peer.id}`)
+            }
         })
         .on('error', (error) => {
             fileInput.disabled = true
@@ -46,83 +48,102 @@ function send(data) {
     else connection.dataChannel.send(JSON.stringify(data))
 }
 
+class FileTransfer {
+    anchor
+    filebuffer = []
+    received = 0
+    ts = Date.now()
+    fileInfo
+    constructor(fileInfo, isSender) {
+        this.fileInfo = fileInfo
+
+        this.anchor = Object.assign(document.createElement('a'), {
+            textContent: fileInfo.name,
+            download: fileInfo.name,
+            role: 'button',
+            className: 'file-link',
+        })
+        // if (isSender)
+        this.anchor.classList.add('secondary')
+        main.append(this.anchor)
+    }
+    setProgress(percent) {
+        percent = Math.max(0, Math.min(100, percent))
+        this.anchor.style.setProperty('--percent', `${percent}%`)
+    }
+    onMessage(message) {
+        const index = new DataView(message.data.slice(0, 4)).getUint32(0, true)
+
+        this.received += message.data.byteLength - 4
+        const percent = Math.round((this.received / this.fileInfo.size) * 100)
+        this.setProgress(percent)
+        const bitrate = formatBytes(Math.round(this.received / (Date.now() - this.ts)) * 1000) + '/s'
+        const ptext = `${formatBytes(this.received)} / ${formatBytes(this.fileInfo.size)}`
+        this.anchor.setAttribute('data-info', ptext)
+        this.anchor.setAttribute('data-info2', bitrate)
+
+        this.filebuffer[index] = message.data.slice(4)
+
+        if (this.received == this.fileInfo.size) {
+            this.anchor.classList.remove('secondary')
+            this.anchor.setAttribute('data-info', ptext + ' ✓')
+            this.anchor.href = URL.createObjectURL(new Blob(this.filebuffer, { type: this.fileInfo.type }))
+            this.anchor.scrollIntoView()
+            send({ event: 'file_end' })
+        }
+    }
+    async sendFile(file) {
+        let start = 0
+        let index = new Uint32Array([0])
+        while (start < file.size) {
+            if (connection.dataChannel.bufferedAmount > max_buffer) {
+                await new Promise((res) => setTimeout(res, 200))
+                continue
+            }
+            const chunk = file.slice(start, start + chunk_size + 1)
+            send({ index, chunk })
+            start += chunk.size
+            index[0]++
+
+            const ptext = `${formatBytes(start)} / ${formatBytes(file.size)}`
+            this.setProgress(Math.round((start / file.size) * 100))
+            this.anchor.setAttribute('data-info', ptext)
+        }
+    }
+}
+
 function onDataChannelMessage(message) {
-    if (typeof message.data != 'string') chunkHandler(message)
+    if (typeof message.data != 'string') REC.onMessage(message)
     else messageHandler(JSON.parse(message.data))
 }
 
 function messageHandler(data) {
     if (data.event == 'file_info') {
-        fileinfo = data.fileinfo
+        REC = new FileTransfer(data.fileinfo, false)
         send({ event: 'file_ready' })
     } else if (data.event == 'file_ready') {
-        send({ event: 'file_start' })
-        fileInput.disabled = true
-        sendFile()
-    } else if (data.event == 'file_start') {
-        filebuffer = []
-        received = 0
-        ts = Date.now()
+        SND = new FileTransfer(getFileInfo(fileInput.files[0]), true)
+        SND.sendFile(fileInput.files[0])
         fileInput.disabled = true
     } else if (data.event == 'file_end') {
         fileInput.disabled = false
     }
 }
 
-function chunkHandler(message) {
-    const index = new DataView(message.data.slice(0, 4)).getUint32(0, true)
-
-    received += message.data.byteLength - 4
-    const progress = Math.round((received / fileinfo.size) * 100)
-    PBAR.style.width = progress + '%'
-    const bitrate = formatBytes(Math.round(received / (Date.now() - ts)) * 1000) + '/s'
-    const ptext = `${formatBytes(received)} / ${formatBytes(fileinfo.size)} - ${bitrate}`
-    progressInfo.textContent = ptext
-
-    filebuffer[index] = message.data.slice(4)
-
-    if (received == fileinfo.size) {
-        const a = document.createElement('a')
-        a.role = 'button'
-        a.textContent = fileinfo.name
-        a.download = fileinfo.name
-        a.href = href = URL.createObjectURL(new Blob(filebuffer, { type: fileinfo.type }))
-        main.append(a)
-        a.scrollIntoView()
-        filebuffer = null
-        send({ event: 'file_end' })
-        fileInput.disabled = false
-    }
-}
-
-async function sendFile() {
-    let start = 0
-    let index = new Uint32Array([0])
-    const file = fileInput.files[0]
-    while (start < file.size) {
-        if (connection.dataChannel.bufferedAmount > max_buffer) {
-            await new Promise((res) => setTimeout(res, 200))
-            continue
-        }
-        send({ index, _: index[0]++, chunk: file.slice(start, (start += chunk_size + 1)) })
-        const progress = Math.round((start / file.size) * 100)
-        PBAR.style.width = progress + '%'
-        const ptext = `${formatBytes(start)} / ${formatBytes(file.size)}`
-        progressInfo.textContent = ptext
-    }
-}
-
 fileInput.addEventListener('change', () => {
     if (!connection) return
     if (!fileInput.files.length) return
-    const file = fileInput.files[0]
-    send({ event: 'file_info', fileinfo: { name: file.name, size: file.size, type: file.type } })
+    send({ event: 'file_info', fileinfo: getFileInfo(fileInput.files[0]) })
 })
+
+function getFileInfo(file) {
+    return { name: file.name, size: file.size, type: file.type }
+}
 
 function appendQRCode(text) {
     const code = document.createElement('div')
     code.id = 'code'
-    // code.append(Object.assign(document.createElement('a'), { href: text, textContent: 'connect' }))
+    code.append(Object.assign(document.createElement('a'), { href: text, textContent: 'connect' }))
     new QRCode(code, {
         text,
         width: 256,
